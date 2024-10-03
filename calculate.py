8bb950da6a12456af516a34b795e0216818a81ae
Mark Nikulin <marknikulin195@gmail.com>
2024-10-03 20:48:05 +0300

    files was modified

--- calculate.py
+++ calculate.py
@@ -29,5 +29,3 @@ if __name__ == "__main__":
 	
 	calc(fig, func, size)
 
-
-
--- triangle.py
+++ triangle.py
@@ -1,6 +1,9 @@
 def area(a, b, c):
+    """ Принимает a, b, c, возвращает полупериметр """
     return (a + b + c) / 2
 
 
 def perimeter(a, b, c):
+    """ Принимает a, b, c, возвращает периметр """
     return a + b + c
+
