6103f93a35287da50aa2e951d7e1e2758bdf2ce3
Kenny <marknikulin195@gmail.com>
2024-11-23 05:21:22 +0300

    calculate.py was modified

--- calculate.py
+++ calculate.py
@@ -1,33 +1,54 @@
-figs = ['circle', 'square', 'triangle']
-funcs = ['perimeter', 'area']
-sizes = {}
+import circle
+import square
+import triangle
 
+# Словарь для хранения функций по фигурам и операциям
+figs = {
+    'circle': {
+        'perimeter': circle.perimeter,
+        'area': circle.area
+    },
+    'square': {
+        'perimeter': square.perimeter,
+        'area': square.area
+    },
+    'triangle': {
+        'perimeter': triangle.perimeter,
+        'area': triangle.area
+    }
+}
 
 def calc(fig, func, size):
-    assert fig in figs
-    assert func in funcs
+    """Вычисляет значение для заданной фигуры и функции."""
+    assert fig in figs, f"Figure '{fig}' is not available."
+    assert func in figs[fig], f"Function '{func}' is not available for figure '{fig}'."
 
-    result = eval(f'{fig}.{func}(*{size})')
+    result = figs[fig][func](*size)  # Вызываем нужную функцию напрямую
 
     return result  # Возвращаем само значение
-
-
-# print(f'{func} of {fig} is {result}')
 
 if __name__ == "__main__":
     func = ''
     fig = ''
-    size = list()
+    size = []
 
+    # Запрашиваем фигуру у пользователя
     while fig not in figs:
-        fig = input(f"Enter figure name, avaliable are {figs}:\n")
+        fig = input(f"Enter figure name, available are {list(figs.keys())}:\n")
 
-    while func not in funcs:
-        func = input(f"Enter function name, avaliable are {funcs}:\n")
+    # Запрашиваем функцию у пользователя
+    while func not in figs[fig]:
+        func = input(f"Enter function name, available are {list(figs[fig].keys())}:\n")
 
-    while len(size) != sizes.get(f"{func}-{fig}", 1):
-        size = list(map(int, input("Input figure sizes separated by space, "
-                                   "1 for circle and square\n").split(' ')))
+    # Запрашиваем размеры в зависимости от фигуры и функции
+    while len(size) != 1:  # Предполагаем, что для круга и квадрата размер один, а для треугольника - три
+        if fig == 'triangle':
+            size = list(map(int, input("Input three sides of the triangle separated by space:\n").split()))
+            if len(size) != 3:
+                print("Please enter exactly three sides for a triangle.")
+                size = []
+        else:
+            size = list(map(int, input("Input figure size (1 for circle and square):\n").split()))
 
     result = calc(fig, func, size)
-# print(f'{func} of {fig} is {result}')
+    print(f'The {func} of the {fig} is {result}.')
