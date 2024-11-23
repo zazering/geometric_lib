9a7ca8f167dfe34e523ff624fbde5cef8589456f
Kenny <marknikulin195@gmail.com>
2024-11-23 04:41:47 +0300

    calculate.py was modified

--- calculate.py
+++ calculate.py
@@ -1,7 +1,3 @@
-import circle
-import square
-import triangle
-
 figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
 sizes = {}
@@ -30,7 +26,8 @@ if __name__ == "__main__":
         func = input(f"Enter function name, avaliable are {funcs}:\n")
 
     while len(size) != sizes.get(f"{func}-{fig}", 1):
-        size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
+        size = list(map(int, input("Input figure sizes separated by space, "
+                                   "1 for circle and square\n").split(' ')))
 
     result = calc(fig, func, size)
 # print(f'{func} of {fig} is {result}')
